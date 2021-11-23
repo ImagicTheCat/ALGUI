4b8d8226ff0a59cba0892e8d42ae1d5f4af4acb0
ImagicTheCat <imagicthecat@gmail.com>
2021-11-23 20:55:02 +0100

    Improve extension GUI.

--- src/ALGUI/ext/GUI.lua
+++ src/ALGUI/ext/GUI.lua
@@ -2,52 +2,43 @@
 -- MIT license (see LICENSE or src/ALGUI/GUI.lua)
 
 local class = require("Luaoop").class
-local GUI = require("ALGUI.GUI")
+local bGUI = require("ALGUI.GUI")
 
-local table_insert = table.insert
+-- A basic GUI implementation with events, widget focus, etc.
+local GUI = class("GUI", bGUI)
 
--- A basic GUI implementation with events/widget focus/etc.
-local eGUI = class("GUI", GUI)
-
--- overed widgets are the last pointer matching widgets displayed in a widget's content
--- x,y: inner content coordinates of widget's parent
--- scale: (optional) absolute scale for current widget
+-- Get overed widgets, i.e. widgets crossed at the pointer position.
+-- (recursive)
+--
+-- list: output list of widgets
+-- x,y: absolute pointer position
 -- return true if the widget is a match
-local function get_overed_widgets(list, widget, x, y, scale)
-  if not scale then scale = 1 end
-
-  if x >= widget.x and y >= widget.y and x <= widget.x+widget.w and y <= widget.y+widget.h then
-    -- append widget's relative x,y and absolute scale
-    table_insert(list, {widget, x-widget.x, y-widget.y, scale})
-
-    if widget.draw_list then
-      -- compute widget's inner content absolute scale and x,y coordinates
-      x,y,scale = (x-widget.x)/widget.zoom-widget.ix, (y-widget.y)/widget.zoom-widget.iy, scale*widget.zoom
+local function getOveredWidgets(list, widget, x, y)
+  if x >= widget.tx and y >= widget.ty and
+      x <= widget.tx+widget.w*widget.tscale and
+      y <= widget.ty+widget.h*widget.tscale then
+    table.insert(list, widget)
       for i=#widget.draw_list,1,-1 do
-        if get_overed_widgets(list, widget.draw_list[i], x, y, scale) then break end -- stop on first match
+        -- stop on first match
+        if getOveredWidgets(list, widget.draw_list[i], x, y) then break end
       end
-    end
-
     return true
   else
     return false
   end
 end
 
--- METHODS
-
-function eGUI:__construct()
-  GUI.__construct(self)
+function GUI:__construct()
+  bGUI.__construct(self)
+  self.pointers_pressed = {} -- map of id => set of widgets
+  self.pointers_overed = {} -- map of id => set of widgets
   -- self.focus
-
-  self.pointers_pressed = {} -- map of id => map of widgets
-  self.pointers_overed = {} -- map of id => map of widgets
 end
 
--- set GUI focus
--- emit "focus_change" on old/new focused widgets
--- widget: can be nil
-function eGUI:setFocus(widget)
+-- Set GUI focus.
+-- Emits "focus-change" on old/new focused widgets.
+-- widget: (optional)
+function GUI:setFocus(widget)
   if self.focus ~= widget then
     if self.focus then self.focus:emit("focus-change", false) end
     self.focus = widget
@@ -55,114 +46,93 @@ function eGUI:setFocus(widget)
   end
 end
 
--- some event emits
+-- Common events interface
 
--- emit "pointer_click" on all overed widgets
-function eGUI:emitPointerClick(id, x, y, code, n)
-  local list = {}
-  get_overed_widgets(list, self, x, y)
-  for _, entry in ipairs(list) do
-    entry[1]:emit("pointer-click", id, entry[2], entry[3], code, n)
+-- Emit "pointer-click" on all overed widgets.
+function GUI:emitPointerClick(id, x, y, code, n)
+  local list = {}; getOveredWidgets(list, self, x, y)
+  for _, widget in ipairs(list) do
+    widget:emit("pointer-click", id, x-widget.tx, y-widget.ty, code, n)
   end
 end
 
--- emit "pointer_press" on all overed widgets
-function eGUI:emitPointerPress(id, x, y, code)
-  local list = {}
-  get_overed_widgets(list, self, x, y)
-
-  -- update pointer's pressed widgets
-  local pressed = {}
-  for _, entry in ipairs(list) do pressed[entry[1]] = true end
-  self.pointers_pressed[id] = pressed
-
-  -- emit event
-  for _, entry in ipairs(list) do
-    entry[1]:emit("pointer-press", id, entry[2], entry[3], code)
+-- Emit "pointer-press" on all overed widgets.
+function GUI:emitPointerPress(id, x, y, code)
+  local list = {}; getOveredWidgets(list, self, x, y)
+  -- Update pointer's pressed widgets and emit event.
+  local pressed = self.pointers_pressed[id]
+  if not pressed then pressed = {}; self.pointers_pressed[id] = pressed end
+  for _, widget in ipairs(list) do
+    pressed[widget] = true
+    widget:emit("pointer-press", id, x-widget.tx, y-widget.ty, code)
   end
 end
 
--- emit "pointer_release" on previously pressed widgets
--- also emit "pointer_click" on previously pressed and still overed widgets
-function eGUI:emitPointerRelease(id, x, y, code)
-  local list = {}
-  get_overed_widgets(list, self, x, y)
-
+-- Emit "pointer-release" on previously pressed widgets and emit
+-- "pointer-click" on previously pressed and still overed widgets.
+function GUI:emitPointerRelease(id, x, y, code)
+  local list = {}; getOveredWidgets(list, self, x, y)
   local pressed = self.pointers_pressed[id]
   if pressed then
     -- emit release event
-    for widget in pairs(pressed) do
-      widget:emit("pointer-release", id, code)
-    end
-
+    for widget in pairs(pressed) do widget:emit("pointer-release", id, code) end
     -- emit click event
-    for _, entry in ipairs(list) do
-      if pressed[entry[1]] then
-        entry[1]:emit("pointer-click", id, entry[2], entry[3], code, 1)
+    for _, widget in ipairs(list) do
+      if pressed[widget] then
+        widget:emit("pointer-click", id, x-widget.tx, y-widget.ty, code, 1)
       end
     end
-
     self.pointers_pressed[id] = nil
   end
 end
 
--- emit "pointer_wheel" on all overed widgets
-function eGUI:emitPointerWheel(id, x, y, amount)
-  local list = {}
-  get_overed_widgets(list, self, x, y)
-  for _, entry in ipairs(list) do
-    entry[1]:emit("pointer-wheel", id, entry[2], entry[3], amount)
+-- Emit "pointer-wheel" on all overed widgets.
+function GUI:emitPointerWheel(id, x, y, amount)
+  local list = {}; getOveredWidgets(list, self, x, y)
+  for _, widget in ipairs(list) do
+    widget:emit("pointer-wheel", id, x-widget.tx, y-widget.ty, amount)
   end
 end
 
--- emit "pointer_move" on all overed widgets
--- also emit "pointer_enter" and "pointer_leave"
-function eGUI:emitPointerMove(id, x, y, dx, dy)
-  local list = {}
-  get_overed_widgets(list, self, x, y)
-
+-- Emit "pointer-move" on all overed widgets.
+-- Also emits "pointer-enter" and "pointer-leave".
+function GUI:emitPointerMove(id, x, y, dx, dy)
+  local list = {}; getOveredWidgets(list, self, x, y)
   -- emit move event
-  for _, entry in ipairs(list) do
-    entry[1]:emit("pointer-move", id, entry[2], entry[3], dx*entry[4], dy*entry[4])
+  for _, widget in ipairs(list) do
+    widget:emit("pointer-move", id, x-widget.tx, y-widget.ty,
+      dx*widget.tscale, dy*widget.tscale)
   end
-
   -- emit enter/leave events
   local old_overed = self.pointers_overed[id] or {}
-  local overed = {}
-  for _, entry in ipairs(list) do overed[entry[1]] = true end
-
+  local overed = {}; for _, widget in ipairs(list) do overed[widget] = true end
   --- enter
   for widget in pairs(overed) do
     if not old_overed[widget] then widget:emit("pointer-enter", id) end
   end
-
   --- leave
   for widget in pairs(old_overed) do
     if not overed[widget] then widget:emit("pointer-leave", id) end
   end
-
   self.pointers_overed[id] = overed
 end
 
--- emit "key_press" on GUI and focused widget
-function eGUI:emitKeyPress(keycode, scancode, repeated)
-  local focus = self.focus
+-- Emit "key-press" on GUI and focused widget.
+function GUI:emitKeyPress(keycode, scancode, repeated)
   self:emit("key-press", keycode, scancode, repeated)
-  if focus then focus:emit("key-press", keycode, scancode, repeated) end
+  if self.focus then self.focus:emit("key-press", keycode, scancode, repeated) end
 end
 
--- emit "key_release" on GUI and focused widget
-function eGUI:emitKeyRelease(keycode, scancode)
-  local focus = self.focus
+-- Emit "key-release" on GUI and focused widget.
+function GUI:emitKeyRelease(keycode, scancode)
   self:emit("key-release", keycode, scancode)
-  if focus then focus:emit("key-release", keycode, scancode) end
+  if self.focus then self.focus:emit("key-release", keycode, scancode) end
 end
 
--- emit "text_input" on GUI and focused widget
-function eGUI:emitTextInput(text)
-  local focus = self.focus
+-- Emit "text-input" on GUI and focused widget.
+function GUI:emitTextInput(text)
   self:emit("text-input", text)
-  if focus then focus:emit("text-input", text) end
+  if self.focus then self.focus:emit("text-input", text) end
 end
 
-return eGUI
+return GUI
